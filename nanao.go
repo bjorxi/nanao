746417cbe5c9b31a5e98bf1d2de35e210d199c80
Nodari Lipartiya <nodari.lipartiya@gmail.com>
2016-07-24 12:23:21 +0300

    added simple command line processing

--- nanao.go
+++ nanao.go
@@ -18,6 +18,27 @@ func welcomeMessage () {
 
 
 func main () {
+  if len(os.Args) < 2 {
+    fmt.Println("Usage: nanao </path/to/file>")
+    return
+  }
+
+  if os.Args[1] == "-v" || os.Args[1] == "--version" {
+    fmt.Println(version)
+    return
+  }
+
+  if os.Args[1] == "-h" || os.Args[1] == "--help" {
+    fmt.Println("Nanao editor", version, "\n")
+    fmt.Println("Usage: nanao </path/to/file>\n")
+    fmt.Println("Arguments:")
+    fmt.Println("\t-v(--version) show the version")
+    fmt.Println("\t-h(--help) show help\n")
+    fmt.Println("Author: Nodari Lipartiya <nodari.lipartiya(at)gmail.com>")
+    fmt.Println("Project url: https://github.com/leemalmac/nanao\n")
+    return
+  }
+
   fileName := os.Args[1]
 
   e := editor.Init()
