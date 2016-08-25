96c5adb9730d14670099503a9b5fdf9c56cf9e56
Nodari Lipartiya <nodari.lipartiya@gmail.com>
2016-08-25 23:27:02 +0300

    config file parsing works

--- editor/config.go
+++ editor/config.go
@@ -7,6 +7,7 @@ import "encoding/json"
 
 
 func (e *Editor) ParseConf(path string) {
+  fmt.Fprintf(os.Stderr,"Parsing %s\n", path)
   configFile, err := ioutil.ReadFile(path)
 
   if err != nil {
@@ -16,9 +17,11 @@ func (e *Editor) ParseConf(path string) {
   fmt.Fprintf(os.Stderr, "%s\n", string(configFile))
   err = json.Unmarshal(configFile, e.conf)
 
+  fmt.Fprintf(os.Stderr, "Tab size %d\n", e.conf.TabSize)
+
   if err != nil {
     fmt.Fprintf(os.Stderr, "Can't Unmarshall %e", err.Error())
   }
 
-  // configFile.Close()
+  configFile.Close()
 }
